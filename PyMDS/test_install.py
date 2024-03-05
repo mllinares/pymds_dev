cd283ee313a102ca836ce50af4db64be9f1ce477
Maureen Llinares <126869078+mllinares@users.noreply.github.com>
2024-03-05 19:21:58 +0100

    Add files via upload

---
+++ PyMDS/test_install.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 13 12:10:21 2023
+
+@author: llinares
+"""
+
+import numpy 
+import matplotlib
+import pyro
+import scipy
+import ruptures
+import torch
+print('Sucess! All libraries installed')
